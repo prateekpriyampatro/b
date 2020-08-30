10d3b757f83686ce409ec4e2566fa7b6985b1e87
prateekpriyampatro <69358225+prateekpriyampatro@users.noreply.github.com>
2020-08-30 20:10:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var bullet,wall;
+var speed;
+var weight,thickness;
+function setup() {
+  createCanvas(1600,400);
+   bullet= createSprite(50,200,50,50);
+   bullet.scale = 0.5;
+   speed = random(223,321);
+   weight = random(35,50);
+   
+   wall = createSprite(700,200,0,200);
+ thickness = random(22,83)
+ bullet.velocityX = speed;
+}
+
+function draw() {
+  background("black");
+  if(c(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      bullet.shapeColor = color(255,0,0);
+    }
+    if(damage<10){
+      bullet.shapeColor = color(0,255,0);
+    }
+  } 
+  
+  drawSprites();
+  
+}
+function c (lbullet,lwall){
+bulletRightEdge = lbullet.x+lbullet.width;
+wallLeftEdge = lwall.x;
+if(bulletRightEdge>=wallLeftEdge){
+  return true ;
+
+}
+else
+ return false;
+}
